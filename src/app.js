f8eb20e4074ebedbe48599d8541de3bf476c078d
Mohd Yusuf <mohdyusuf0735@gmail.com>
2021-11-04 19:35:14 +0530

    heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -62,6 +63,6 @@ app.get('*', (req, res) => {
     res.send('my 404 page')
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
